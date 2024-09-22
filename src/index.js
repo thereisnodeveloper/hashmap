210b04aab7b028cd13f09fe0bc19b2bcac3a8a4c
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-09-22 12:05:09 -0400

    add HashMap class, initialize structure

--- src/index.js
+++ src/index.js
@@ -21,10 +21,30 @@ class HashMap {
   }
 
   set(key, value) {
-    // find key using hash code
+    // find bucket using hash code
+    // traverse linkedList until you find key
     // check key against existingKey
-      // if same, overwrite old value
+    // check fn_growBucketIfNeeded
+    // if same, overwrite old value with $value
+    // if different key (still same bucket), create new node in linkedList
   }
+
+  get(key) {
+    // TODO: get(key) takes one argument as a key and returns the value that is assigned to this key. If a key is not found, return null.
+  }
+  // TODO: has(key) takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
+
+  // TODO: remove(key) takes a key as an argument. If the given key is in the hash map, it should remove the entry with that key and return true. If the key isn’t in the hash map, it should return false.
+
+  // TODO: length() returns the number of stored keys in the hash map.
+
+  // TODO: clear() removes all entries in the hash map.
+
+  // TODO: keys() returns an array containing all the keys inside the hash map.
+
+  // TODO: values() returns an array containing all the values.
+
+  // TODO: entries() returns an array that contains each key, value pair. Example: [[firstKey, firstValue], [secondKey, secondValue]]
 }
 
 const hashMap1 = new HashMap(16);
